8ce943bf66593404c267ef87ffdf743a138e6e4a
YashPatil2602 <patilyash2110@gmail.com>
2026-08-27 22:08:39 +0530

    Apply premium gold theme across website

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import "./App.css";
 
 import App from "./App";
+import "./components/PremiumGold.css";
 
 createRoot(document.getElementById("root")).render( <
     StrictMode >
